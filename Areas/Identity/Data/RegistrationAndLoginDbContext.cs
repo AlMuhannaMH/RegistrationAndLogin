60cb79d3b8ea147611da0b0ea70dbad6ba170b06
Mohammed AlMuhanna <AlMuhannaMH@Outlook.com>
2020-10-08 00:52:42 +0300

    First Release

---
+++ Areas/Identity/Data/RegistrationAndLoginDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RegistrationAndLogin.Areas.Identity.Data;
+
+namespace RegistrationAndLogin.Data
+{
+    public class RegistrationAndLoginDbContext : IdentityDbContext<AppUser>
+    {
+        public RegistrationAndLoginDbContext(DbContextOptions<RegistrationAndLoginDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RegistrationAndLogin.Areas.Identity.Data;
+using RegistrationAndLogin.Data;
+
+[assembly: HostingStartup(typeof(RegistrationAndLogin.Areas.Identity.IdentityHostingStartup))]
+namespace RegistrationAndLogin.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<RegistrationAndLoginDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("RegistrationAndLoginDbContextConnection")));
+
+                services.AddDefaultIdentity<AppUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    
+                 })
+                    .AddEntityFrameworkStores<RegistrationAndLoginDbContext>();
+            });
+        }
+    }
+}
